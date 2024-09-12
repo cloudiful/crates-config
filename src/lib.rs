90f69d812540f70ab1f1156aee20dd64ffc68ad6
Cloudiful <45996018+cloudiful@users.noreply.github.com>
2024-09-12 14:07:10 +0800

    use generics

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,43 @@
-use std::fs;
-use std::path::PathBuf;
-
 use log::warn;
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::{Path, PathBuf};
 
-// overall config
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Config;
+pub struct Config {
+    pub path: PathBuf,
+}
 
 impl Config {
-    pub fn save(c: &Config) {
-        fs::create_dir_all("config").expect("creating config dir failed");
-        fs::File::create("config/config.toml").expect("creating config file failed");
-        let toml_string = toml::to_string_pretty(c).expect("toml to string failed");
-        fs::write("config/config.toml", toml_string).expect("writing config file failed");
+    /// save current config to a toml file
+    pub fn save(self) {
+
+        // if dir not exists, create one
+        match &self.path.parent() {
+            None => {}
+            Some(dir) => { fs::create_dir_all(dir).expect("creating config dir failed"); }
+        }
+
+        // create file
+        fs::File::create(&self.path).expect("creating config file failed");
+
+        // stringify and write
+        let toml_string = toml::to_string_pretty(&self).expect("toml to string failed");
+        fs::write(&self.path, toml_string).expect("writing config file failed");
     }
 
-    pub fn read(path: &str) -> Option<toml::Value> {
-        let config_file = PathBuf::from(path);
+    /// Read config from a toml file
+    pub fn read<T>(self) -> Option<T>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let config_file = &self.path;
 
         let f = fs::read_to_string(config_file);
 
         match f {
             Ok(content) => {
-                let config: toml::Value = toml::from_str(&content).expect("parsing toml error");
+                let config: T = toml::from_str(&content).expect("parsing toml error");
                 Some(config)
             }
             Err(e) => {
