d11ed8e0bd90aa21c7cf10e9db5047774bf11cd3
Cloudiful <bunnies-spender.0h@icloud.com>
2025-02-10 01:36:19 +0800

    use generics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use log::info;
+use std::collections::HashMap;
 use config::Config;
+use log::info;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 fn main() {
-    let config = Config::read("Cargo.toml").unwrap();
-    info!("{:?}", config);
+    let config = Config{
+        path: PathBuf::from("Cargo.toml"),
+    };
+    let res = config.read().unwrap();
+    info!("{:#?}", res);
 }
